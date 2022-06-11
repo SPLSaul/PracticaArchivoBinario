bf877c162b15aa3892f90820e94b82dac617d35d
Saul Perez Lopez <drsaul1984@gmail.com>
2022-06-10 17:00:36 -0700

    Examen Corregido

--- PracticaArchivoBinario/Program.cs
+++ PracticaArchivoBinario/Program.cs
@@ -9,218 +9,79 @@ namespace PracticaArchivoBinario
 {
     class Program
     {
-        class ArchivoBinarioEmpleados
-        {
-            //declaracion de flujos
-            BinaryWriter bw = null; //flujo salida - escritura de datos
-            BinaryReader br = null; //flujo entrada - lectura de datos
-                                    //campos de la clase
-            string Nombre, Direccion, NomArch;
-            long Telefono;
-            int NumEmp;
-            short DiasTrabajados;
-            float SalarioDiario;
-            public void CrearArchivo(string Archivo)
-            {
-                NomArch = Archivo;
-                char resp; //variable local método
-                try
-                {
-                    //creación del flujo para escribir datos al archivo
-                    bw = new BinaryWriter(new FileStream(Archivo, FileMode.Create, FileAccess.Write));
-
-                    //captura de datos
-                    do
-                    {
-                        Console.Clear();
-                        Console.Write("Numero del Empleado: ");
-                        NumEmp = Int32.Parse(Console.ReadLine());
-                        Console.Write("Nombre del Empleado: ");
-                        Nombre = Console.ReadLine();
-                        Console.Write("Dirección del Empleado: ");
-                        Direccion = Console.ReadLine();
-                        Console.Write("Teléfono del Empleado: ");
-                        Telefono = Int64.Parse(Console.ReadLine());
-                        Console.Write("Dias Trabajados del Empleado: ");
-                        DiasTrabajados = Int16.Parse(Console.ReadLine());
-                        Console.Write("Salario Diario del Empleado: ");
-                        SalarioDiario = Single.Parse(Console.ReadLine());
-
-                        //escribe los datos al archivo en disco
-                        bw.Write(NumEmp);
-                        bw.Write(Nombre);
-                        bw.Write(Direccion);
-                        bw.Write(Telefono);
-                        bw.Write(DiasTrabajados);
-                        bw.Write(SalarioDiario);
-                        Console.Write("\n\nDeseas Almacenar otro Registro (s/n) ? ");
-                        resp = Char.Parse(Console.ReadLine());
-
-                    } while ((resp == 's') || (resp == 'S'));
-                }
-                catch (IOException es)
-                {
-                    Console.WriteLine("\nError : " + es.Message);
-                    Console.WriteLine("\nRuta : " + es.StackTrace);
-                }
-                finally
-                {
-                    if (bw != null) bw.Close(); //cierra flujo escritura
-                    Console.Write("\nPresione <enter> para terminar la Escritura de Datos y regresar al Menu.");
-                    Console.ReadKey();
-                }
-            }
-            public void MostrarArchivo(string Archivo)
-            {
-                NomArch = Archivo;
-                try
-                {
-                    //verifica si existe el archivo
-                    if (File.Exists(Archivo))
-                    {
-                        //creación flujo para leer datos del archivo
-                        br = new BinaryReader(new FileStream(Archivo, FileMode.Open, FileAccess.Read));
-
-                        //despliegue de datos en pantalla
-                        Console.Clear();
-                        do
-                        {
-                            //lectura de registros mientras no llegue al EndOfFile
-                            NumEmp = br.ReadInt32();
-                            Nombre = br.ReadString();
-                            Direccion = br.ReadString();
-                            Telefono = br.ReadInt64();
-                            DiasTrabajados = br.ReadInt16();
-                            SalarioDiario = br.ReadSingle();
-                            //muestra los datos en pantalla
-
-                            Console.WriteLine("Numero del Empleado : " + NumEmp);
-                            Console.WriteLine("Nombre del Empleado : " + Nombre);
-                            Console.WriteLine("Dirección del Empleado : " + Direccion);
-                            Console.WriteLine("Teléfono del Empleado : " + Telefono);
-                            Console.WriteLine("Dias Trabajados del Empleado : " + DiasTrabajados);
-                            Console.WriteLine("Salario Diario del Empleado: {0:C} ", SalarioDiario);
-                            Console.WriteLine("SUELDO TOTAL del Empleado: {0:C}", (DiasTrabajados * SalarioDiario));
-                            Console.WriteLine("\n");
-                        } while (true);
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        Console.WriteLine("\n\nEl Archivo " + Archivo + " No Existe en el Disco!!");
-                        Console.Write("\nPresione <enter> para Continuar...");
-                        Console.ReadKey();
-                    }
-                }
-                catch (EndOfStreamException)
-                {
-                    Console.WriteLine("\n\nFin del Listado de Empleados");
-                    Console.Write("\nPresione <enter> para Continuar...");
-                    Console.ReadKey();
-                }
-                finally
-                {
-                    if (br != null) br.Close(); //cierra flujo lectura
-                    Console.Write("\nPresione <enter> para terminar la Lectura de Datos y regresar al Menu.");
-                    Console.ReadKey();
-                }
-            }
-            ~ArchivoBinarioEmpleados()
-            {
-                Console.WriteLine("\nMemoria Liberada Archivo Binario: " + this.NomArch);
-            }
-        }
         static void Main(string[] args)
         {
-            //declaración variables auxiliares
-            string Arch = null;
-            short opcion;
-            opcion = 0;
-
-            //creación del objeto
-            ArchivoBinarioEmpleados A1 = new ArchivoBinarioEmpleados();
-
-            //Menu de Opciones
-            do
+            Monster monster = new Monster();
+            monster.Captura();
+            
+            try                                  //Creacion de archivo 
             {
-                Console.Clear();
-                Console.WriteLine("\n*** ARCHIVO BINARIO EMPLEADOS***");
-                Console.WriteLine("1.- Creación de un Archivo.");
-                Console.WriteLine("2.- Lectura de un Archivo.");
-                Console.WriteLine("3.- Salida del Programa.");
-                Console.Write("\nQue opción deseas: ");
+                StreamWriter escritura = new StreamWriter("Inventario.txt",true);
 
-                try
+                escritura.WriteLine(monster.Imprimir());   //El objeto escritura se encarga de escribir lo que el metodo Imprimir nos esta regresando
+                escritura.Close();                         //Despues de escribir, se cierra el flujo de datos hacia el archivo
+
+            }
+            catch(IOException e) 
+            {
+                Console.WriteLine($"Error en el documento: {e.Message}");
+            }           
+            try
+            {
+                StreamReader lector = new StreamReader("Inventario.txt",true);      //Creacion de archivo para lectura
+                int c = lector.Read();   //c se encarga de contar el numero de caracteres dentro del archivo
+                while (c != -1)                   //Mientras c no sea igual al ultimo caracter
                 {
-                    opcion = Int16.Parse(Console.ReadLine());
-                    switch (opcion)
-                    {
-                        case 1:
-                            //bloque de escritura
-                            try
-                            {
-                                //captura nombre archivo
-                                Console.Write("\nAlimenta el Nombre del Archivo a Crear: ");
-                                Arch = Console.ReadLine();
-
-                                //verifica si esxiste el archivo
-                                char resp = 's';
-                                if (File.Exists(Arch))
-                                {
-                                    Console.Write("\nEl Archivo Existe!!, Deseas Sobreescribirlo(s / n) ? ");
-                                    resp = Char.Parse(Console.ReadLine());
-                                }
-                                if ((resp == 's') || (resp == 'S'))
-                                    A1.CrearArchivo(Arch);
-                            }
-                            catch (IOException e)
-                            {
-                                Console.WriteLine("\nError : " + e.Message);
-
-                                Console.WriteLine("\nRuta : " + e.StackTrace);
-                            }
-                            break;
-
-                        case 2:
-                            //bloque de lectura
-                            try
-                            {
-                                //captura nombre archivo
-                                Console.Write("\nAlimenta el Nombre del Archivo que deseas Leer: ");
-                                Arch = Console.ReadLine();
-                                A1.MostrarArchivo(Arch);
-                            }
-                            catch (IOException e)
-                            {
-                                Console.WriteLine("\nError : " + e.Message);
-                                Console.WriteLine("\nRuta : " + e.StackTrace);
-                            }
-                            break;
-
-                        case 3:
-                            Console.Write("\nPresione <enter> para Salir del Programa.");
-                            Console.ReadKey();
-                            break;
-
-                        default:
-                            Console.Write("\nEsa Opción No Existe!!, Presione < enter > para Continuar...");
-                            Console.ReadKey();
-                            break;
-                    }
+                    c = lector.Read();              //c = numero de carcteres dentro del archivo
+                    char letra = (char)c;                //letras nos convierte a letra los caraceteres dentro del documento
+                    Console.Write(letra);
                 }
-                catch (FormatException f)
-                {
-                    Console.WriteLine("\nMensaje del error: " + f.Message);
-                    Console.WriteLine("\nRuta del error: " + f.StackTrace);
-                    Console.WriteLine("\npresione <enter> para continuar...");
-                    Console.ReadKey();
-                }
-                finally
-                {
-                    Console.WriteLine("\npresione <enter> para continuar...");
-                    Console.ReadKey();
-                }
-            } while (opcion != 3);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error en el archivo {e.Message}");
+            }
+        }                                                          
+    }
+    class Limpieza
+    {
+        public void Limpiar()
+        {
+            Console.WriteLine("Presione cualquier tecla para continuar: ");
+            Console.ReadKey();
+            Console.Clear();
         }
+    }
+    class Monster:Limpieza
+    {
+        string nombre,descripcion,resultados;
+        float precio;
+        int cantidad;        
+        public void Captura()
+        {
+            Console.Write("Ingresa el nombre del producto: ");
+            nombre = Console.ReadLine();
+            Limpiar();
+            Console.Write("Ingresa la descripcion del producto: ");
+            descripcion = Console.ReadLine();
+            Limpiar();
+            try
+            {
+                Console.Write("Ingresa el precio del producto: ");
+                precio = float.Parse(Console.ReadLine());
+                Limpiar();
+                Console.Write("Ingresa la cantidad de productos en existencia: ");
+                cantidad = int.Parse(Console.ReadLine());
+                Limpiar();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error en la captura, trata de nuevo con un numero valido.\n{e.Message}");
+            }            
+        }
+        public string Imprimir()
+        {
+             return resultados= ($"Producto: {nombre}         Precio: {precio:C}\nDespricion: {descripcion}\nCantidad en existencia: {cantidad}");            
+        }        
     }
 }
